312d15dcbe197c199a6b0e7241b366d529e51fd1
anamfazal <anamfazal94@gmail.com>
2020-08-28 20:04:37 +0530

    REFACTORED: code to use one wage builder for all companies

--- EmployeeWageProblem/EmployeeWageBuilder.java
+++ EmployeeWageProblem/EmployeeWageBuilder.java
@@ -1,17 +1,13 @@
-/* PURPOSE: Calculates and saves totalwages for multiple companies  using instance variables */
+/*PURPOSE: Refactor to have one EmployeeWageBuilder for multiple companies.Use array to store company objects */
 
 public class EmployeeWageBuilder {
 	
-	//CONSTANTS
-	public static final int IS_FULL_TIME=2;
-	public static final int IS_PART_TIME=1;  
-	
 	//INSTANCE VARIABLES
-	private final String company;
-	private final int empRatePerHour;
-	private final int maxWorkingDays;
-	private final int maxMonthlyHours;
-	private int totalEmpWage;
+	public final String company;
+	public final int empRatePerHour;
+	public final int maxWorkingDays;
+	public final int maxMonthlyHours;
+	public int totalEmpWage;
 	
 	//CONSTRUCTOR
 	public EmployeeWageBuilder(String company,int empRatePerHour, int maxWorkingDays, int maxMonthlyHours) {
@@ -21,61 +17,18 @@ public class EmployeeWageBuilder {
 		this.maxWorkingDays=maxWorkingDays;
 		this.maxMonthlyHours=maxMonthlyHours;
 	}
-	
 	//INSTANCE METHOD
-	public void computeWage() {
-
-		//LOCAL VARIABLES
-      		int workHours=0;
-      		int totalWorkHours=0;
-      		int totalWorkingDays=0;
-
-		//while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
-      		while (totalWorkHours <= maxMonthlyHours && totalWorkingDays <maxWorkingDays) {
-
-      			totalWorkingDays++;                     			       //Increment working days logged till now
-      			int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;         //Get attendance
-
-      			switch (attendanceStatus) {             			       //Condition check for attendance duration
-
-        			case IS_FULL_TIME:
-					workHours=8;
-             	    			break;
-         			case IS_PART_TIME:
-            	    			workHours=4;
-            				break;
-               		 	default:
-         		    		workHours=0;
-
-        	}
-
-     	    	totalWorkHours+=workHours;  	                           		//total working hours logged upto current day
-     		System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
-
-        }
-
-      	totalEmpWage=totalWorkHours * empRatePerHour;  	                                //Calculate month's wages
-
+	public void setTotalWage(int totalEmpWage) {
+		this.totalEmpWage=totalEmpWage;
 	}
 	
 	@Override
 	public String toString() {
-		return ("Total employee wage for "+company+" is "+totalEmpWage);       //Print final monthly wage with company name
+		return ("Total employee wage for "+company+" is "+totalEmpWage);     //Print final monthly wage with company name
 	}
 	
-	public static void main(String[] args) {
-	
-		//Create two company objects
-		EmployeeWageBuilder Company1= new EmployeeWageBuilder("DMart",20,2,10);
-		EmployeeWageBuilder Company2= new EmployeeWageBuilder("Reliance",30,4,12);
-		
-		//Call instance method "computeWage" that computes monthly wages for companies for these objects
-		Company1.computeWage();
-		System.out.println(Company1);  					       
-		Company2.computeWage();
-		System.out.println(Company2);
-	}
 
 }
+
 
 
---
+++ EmployeeWageProblem/WageCalculation.java
@@ -0,0 +1,71 @@
+/*PURPOSE: Refactor to have one EmployeeWageBuilder for multiple companies.Use array to store company objects */
+
+public class WageCalculation {
+
+	//CONSTANTS
+	public static final int IS_FULL_TIME=2;
+	public static final int IS_PART_TIME=1;  
+	//INSTANCE VARIABLES 
+	private int companyNumber=0;
+	//ARRAY declaration
+	private EmployeeWageBuilder[] companyArray;   
+	
+	
+	//DEFAULT CONSTRUCTOR: array object created with size 5
+	public WageCalculation() {
+		companyArray=new EmployeeWageBuilder[5];   //Array initialized
+	} 
+	
+	//INSTANCE METHODS
+	private void addCompanyDetails(String company,int empRatePerHour, int maxWorkingDays, int maxMonthlyHours) { //create new company object with parameters and store the reference of object in current array index
+		companyArray[companyNumber]=new EmployeeWageBuilder(company,empRatePerHour,maxWorkingDays,maxMonthlyHours); 
+		companyNumber++;    //increment number of company counter
+	}
+	private void computeEmployeeWage() {  //for each company object, call method that sets total wage fot it.As parameter,give computed wage of current object.
+		for (int i=0; i<companyNumber; i++) {
+			companyArray[i].setTotalWage(this.computeEmployeeWage(companyArray[i]));
+			System.out.println(companyArray[i]);   //implements according to overridden toString() method  
+		}
+	}
+	
+	private int computeEmployeeWage(EmployeeWageBuilder wageBuilderElement) {  //called by computeEmployeeWage() with current company object reference as parameter
+		//LOCAL VARIABLES
+		int workHours=0;
+      		int totalWorkHours=0;
+      		int totalWorkingDays=0;
+
+        	//COMPUTATION: while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
+      		while (totalWorkHours <= wageBuilderElement.maxMonthlyHours && totalWorkingDays <wageBuilderElement.maxWorkingDays) {
+
+      			totalWorkingDays++;                     			       //Increment working days logged till now
+      			int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;         //Get attendance
+
+      			switch (attendanceStatus) {             			       //Condition check for attendance duration
+
+        			case IS_FULL_TIME:
+       					workHours=8;
+                  			break;
+         			case IS_PART_TIME:
+            	        		workHours=4;
+            				break;
+                		default:
+         	  			workHours=0;
+
+            		}
+
+     	    	totalWorkHours+=workHours;  	                           	      //total working hours logged upto current day
+     		System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
+
+        	}
+      		return totalWorkHours * wageBuilderElement.empRatePerHour;                    //Calculate month's wages
+        
+	}
+	public static void main(String[] args) {
+		WageCalculation wageBuilder= new WageCalculation();			      //create object and initialize to given parameters
+		wageBuilder.addCompanyDetails("DMart",20,2,10);
+		wageBuilder.addCompanyDetails("Reliance",20,4,20);
+		wageBuilder.computeEmployeeWage();				      //call to computation method
+
+	}
+
+}
