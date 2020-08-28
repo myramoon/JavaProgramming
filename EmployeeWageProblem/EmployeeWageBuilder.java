be38198308e0c3a63942d6b3f766c34fc672293b
anamfazal <anamfazal94@gmail.com>
2020-08-28 19:48:39 +0530

    REFACTORED:Creates objects for multiple companies

--- EmployeeWageProblem/EmployeeWageBuilder.java
+++ EmployeeWageProblem/EmployeeWageBuilder.java
@@ -1,4 +1,4 @@
-/* PURPOSE: Calculates wages for multiple companies in Procedural way using CLASS methods */
+/* PURPOSE: Calculates and saves totalwages for multiple companies  using instance variables */
 
 public class EmployeeWageBuilder {
 	
@@ -6,49 +6,76 @@ public class EmployeeWageBuilder {
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;  
 	
-	//STATIC METHOD
-	public static void computeWage(String company,int empRatePerHour,int maxWorkingDays,int maxMonthlyHours) {
+	//INSTANCE VARIABLES
+	private final String company;
+	private final int empRatePerHour;
+	private final int maxWorkingDays;
+	private final int maxMonthlyHours;
+	private int totalEmpWage;
+	
+	//CONSTRUCTOR
+	public EmployeeWageBuilder(String company,int empRatePerHour, int maxWorkingDays, int maxMonthlyHours) {
+	
+		this.company=company;
+		this.empRatePerHour=empRatePerHour;
+		this.maxWorkingDays=maxWorkingDays;
+		this.maxMonthlyHours=maxMonthlyHours;
+	}
+	
+	//INSTANCE METHOD
+	public void computeWage() {
 
-		//VARIABLES
-		int workHours=0;
-		int totalWorkHours=0;
-		int totalWorkingDays=0;
+		//LOCAL VARIABLES
+      	int workHours=0;
+      	int totalWorkHours=0;
+      	int totalWorkingDays=0;
 
 		//while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
-		while (totalWorkHours <= maxMonthlyHours && totalWorkingDays <maxWorkingDays) {
+      	while (totalWorkHours <= maxMonthlyHours && totalWorkingDays <maxWorkingDays) {
 
-			totalWorkingDays++;                     			       //Increment working days logged till now
-			int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;         //Get attendance
+      		totalWorkingDays++;                     			       //Increment working days logged till now
+      		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;         //Get attendance
 
-			switch (attendanceStatus) {             			       //Condition check for attendance duration
+      		switch (attendanceStatus) {             			       //Condition check for attendance duration
 
-				case IS_FULL_TIME:
+        		case IS_FULL_TIME:
 					workHours=8;
-					break;
-				case IS_PART_TIME:
-					workHours=4;
-					break;
-				default:
-					workHours=0;
-			}
+             	    break;
+         		case IS_PART_TIME:
+            	    workHours=4;
+            		break;
+                default:
+         		    workHours=0;
 
-			totalWorkHours+=workHours;  	                           		//total working hours logged upto current day
-			System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
+        	}
 
-		}
+     	    totalWorkHours+=workHours;  	                           		//total working hours logged upto current day
+     		System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
 
-		int totalEmpWage=totalWorkHours * empRatePerHour;                               //Calculate month's wages
-		System.out.println("Total employee wage for "+company+" is "+totalEmpWage);     //Print final monthly wage with company name
+        }
 
+      	totalEmpWage=totalWorkHours * empRatePerHour;  	                                //Calculate month's wages
+
+	}
+	
+	@Override
+	public String toString() {
+		return ("Total employee wage for "+company+" is "+totalEmpWage);       //Print final monthly wage with company name
 	}
 	
 	public static void main(String[] args) {
 	
-		computeWage("DMart",20,2,10);                                                   //call to static method that computes monthly wages for companies
-		computeWage("BigBasket",10,4,20);
+		//Create two company objects
+		EmployeeWageBuilder Company1= new EmployeeWageBuilder("DMart",20,2,10);
+		EmployeeWageBuilder Company2= new EmployeeWageBuilder("Reliance",30,4,12);
+		
+		//Call instance method "computeWage" that computes monthly wages for companies for these objects
+		Company1.computeWage();
+		System.out.println(Company1);  					       
+		Company2.computeWage();
+		System.out.println(Company2);
 	}
 
 }
-
 
 
