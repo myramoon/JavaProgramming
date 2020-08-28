4672d753d387684b08c30b86c3d0dc1d3c67d175
anamfazal <anamfazal94@gmail.com>
2020-08-28 19:34:49 +0530

    REFACTORED:Computes wages for multiple companies using procedural way

--- EmployeeWageProblem/EmployeeWageBuilder.java
+++ EmployeeWageProblem/EmployeeWageBuilder.java
@@ -1,55 +1,54 @@
-/* PURPOSE: Calculates wages in a class method */
+/* PURPOSE: Calculates wages for multiple companies in Procedural way using CLASS methods */
 
 public class EmployeeWageBuilder {
 	
 	//CONSTANTS
 	public static final int IS_FULL_TIME=2;
 	public static final int IS_PART_TIME=1;  
-	public static final int EMP_RATE_PER_HOUR=20;
-	public static final int MAX_WORKING_DAYS=20;
-	public static final int MAX_MONTHLY_HOURS=100;
 	
 	//STATIC METHOD
-	public static void computeWage() {
-	//VARIABLES
-      	int workHours=0;
-      	int totalWorkHours=0;
-      	int totalWorkingDays=0;
+	public static void computeWage(String company,int empRatePerHour,int maxWorkingDays,int maxMonthlyHours) {
 
-	//while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
-      	while (totalWorkHours <= MAX_MONTHLY_HOURS && totalWorkingDays <MAX_WORKING_DAYS) {
+		//VARIABLES
+		int workHours=0;
+		int totalWorkHours=0;
+		int totalWorkingDays=0;
 
-      		totalWorkingDays++;                     			//Increment working days counter
-      		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;  //Get attendance
+		//while loop to calculate work hours for each day until either 100hours are done or 20 days are reached
+		while (totalWorkHours <= maxMonthlyHours && totalWorkingDays <maxWorkingDays) {
 
-      		switch (attendanceStatus) {             			//Condition check for 0 or 1
+			totalWorkingDays++;                     			       //Increment working days logged till now
+			int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;         //Get attendance
 
-        		case IS_FULL_TIME:
-                		workHours=8;
-               			break;
-         		case IS_PART_TIME:
-            			workHours=4;
-            			break;
-              	 	default:
-         		    	workHours=0;
-		}
+			switch (attendanceStatus) {             			       //Condition check for attendance duration
 
-     		totalWorkHours+=workHours;                   	   //total working hours logged upto current day
-     		System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
-		}
+				case IS_FULL_TIME:
+					workHours=8;
+					break;
+				case IS_PART_TIME:
+					workHours=4;
+					break;
+				default:
+					workHours=0;
+			}
 
-      	    	int totalEmpWage=totalWorkHours * EMP_RATE_PER_HOUR;                      //Calculate month's wages
-            	System.out.println("Total employee wage for the month : "+totalEmpWage);  //Print final monthly wage
+			totalWorkHours+=workHours;  	                           		//total working hours logged upto current day
+			System.out.println("Day number: "+ totalWorkingDays + "  Emp Hr: "+ workHours);
 
 		}
+
+		int totalEmpWage=totalWorkHours * empRatePerHour;                               //Calculate month's wages
+		System.out.println("Total employee wage for "+company+" is "+totalEmpWage);     //Print final monthly wage with company name
+
+	}
+	
 	public static void main(String[] args) {
-
-		computeWage(); 		                                                          //call to static method that computes monthly wages
+	
+		computeWage("DMart",20,2,10);                                                   //call to static method that computes monthly wages for companies
+		computeWage("BigBasket",10,4,20);
 	}
 
 }
-
-
 
 
 
