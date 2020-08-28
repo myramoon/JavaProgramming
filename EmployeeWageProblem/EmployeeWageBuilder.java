f0b135a98ddc02abfc93fa16835d2ddc85961dc0
anamfazal <anamfazal94@gmail.com>
2020-08-28 17:53:02 +0530

    Incudes part time condition in DayWageUC

--- EmployeeWageProblem/EmployeeWageBuilder.java
+++ EmployeeWageProblem/EmployeeWageBuilder.java
@@ -1,25 +1,30 @@
+/* PURPOSE: Adds part time employee and wage */
 public class EmployeeWageBuilder {
 	
 	//CONSTANTS
-	static final int IS_FULL_TIME=1;  
+	static final int IS_FULL_TIME=2;
+	static final int IS_PART_TIME=1;  
 	static final int EMP_RATE_PER_HOUR=20;
-	
-	public static void main(String[] args) {
 
+	public static void main(String[] args) {
 		//VARIABLES
 		int workHours;
 		int empWage;
 
-		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 2; //Get attendance as 0 or 1 for absent and present respectively
-		if (attendanceStatus == IS_FULL_TIME)			       //Condition check for 0 or 1
+		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 3;  
+		if (attendanceStatus == IS_FULL_TIME)		               
 			workHours=8;
+		else if (attendanceStatus == IS_PART_TIME)
+			workHours=4;
 		else
 			workHours=0;
-
-		empWage=workHours * EMP_RATE_PER_HOUR; 				//wage calculation
+		
+		empWage=workHours * EMP_RATE_PER_HOUR; 				
 		System.out.println("Employee wage for the day is : "+empWage);
-
 	}
 
 }
+
+
+
 
