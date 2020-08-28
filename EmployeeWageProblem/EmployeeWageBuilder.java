b0fa52087253d19eeb7bdfaae4d7eca7e9babd00
anamfazal <anamfazal94@gmail.com>
2020-08-28 17:24:47 +0530

    Computes wage for 1 day

--- EmployeeWageProblem/EmployeeWageBuilder.java
+++ EmployeeWageProblem/EmployeeWageBuilder.java
@@ -1,13 +1,24 @@
 public class EmployeeWageBuilder {
-
-	static final int IS_FULL_TIME=1;  //CONSTANT
-
+	
+	//CONSTANTS
+	static final int IS_FULL_TIME=1;  
+	static final int EMP_RATE_PER_HOUR=20;
+	
 	public static void main(String[] args) {
+
+		//VARIABLES
+		int workHours;
+		int empWage;
+
 		int attendanceStatus=(int) Math.floor(Math.random() * 10) % 2; //Get attendance as 0 or 1 for absent and present respectively
-		if (attendanceStatus == IS_FULL_TIME)				           //Condition check for 0 or 1
-			System.out.println("Employee is present");
+		if (attendanceStatus == IS_FULL_TIME)			       //Condition check for 0 or 1
+			workHours=8;
 		else
-			System.out.println("Employee is absent");
+			workHours=0;
+
+		empWage=workHours * EMP_RATE_PER_HOUR; 				//wage calculation
+		System.out.println("Employee wage for the day is : "+empWage);
+
 	}
 
 }
